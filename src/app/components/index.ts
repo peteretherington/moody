50abd1ae120ed72c37cfe9a409534e94f426ea3f
Peter Etherington <pjg.etherington@gmail.com>
2019-09-17 14:26:35 -0400

    Gutted ToDo app and cleaned slate for Moody

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,5 +1,3 @@
 export { Footer } from './Footer';
 export { Header } from './Header';
-export { TodoList } from './TodoList';
-export { TodoItem } from './TodoItem';
-export { TodoTextInput } from './TodoTextInput';
+export { Playlist } from './Playlist';
--- src/app/reducers/state.ts
+++ src/app/reducers/state.ts
@@ -1,10 +1,5 @@
-import { TodoModel } from 'app/models';
-
 export interface RootState {
-  todos: RootState.TodoState;
   router?: any;
 }
 
-export namespace RootState {
-  export type TodoState = TodoModel[];
-}
+export namespace RootState {}
